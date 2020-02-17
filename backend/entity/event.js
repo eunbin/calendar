3f4fff5312f04c2a7b2fa441a1aacd8f425772ce
Eunbeen Ahn <ahneunbin@gmail.com>
2020-02-18 01:36:24 +0900

    refactor: Date 클래스 추가

--- backend/entity/event.js
+++ backend/entity/event.js
@@ -2,12 +2,18 @@ import { uuid } from "../util/util"
 import { dateFormat } from "../constant/date";
 import moment from "moment"
 
+class Date {
+  constructor({ date, dateTime }) {
+    this.date = date
+    this.dateTime = dateTime
+  }
+}
 class Event {
   constructor (obj) {
     this.id = uuid();
     this.title = obj.title;
-    this.start = obj.start;
-    this.end = obj.end;
+    this.start = new Date(obj.start);
+    this.end = new Date(obj.end);
     this.createdAt = moment().format(dateFormat.DATE_TIME);
   }
 }
