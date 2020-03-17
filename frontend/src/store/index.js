b8a054585ad00b9bfca60dcf6fb79de00f779dd9
Eunbeen Ahn <ahneunbin@gmail.com>
2020-03-17 21:55:53 +0900

    refactor: API 호출 에러 객체 공통화 및 서버 비정상 에러 핸들링 추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,6 +6,11 @@ import moment from 'moment'
 
 Vue.use(Vuex)
 
+const createError = (e) => ({
+  result: false,
+  message: (e.response && e.response.data) || e
+})
+
 const store = new Vuex.Store({
   state: {
     eventsList: [],
@@ -49,7 +54,7 @@ const store = new Vuex.Store({
         }, {})
         commit('SET_EVENTS_MAP', res)
       } catch (e) {
-        return (e.response && e.response.data) || e
+        setTimeout(() => alert(createError(e).message))
       }
     },
     async addEvent ({ commit }, newEvent) {
@@ -57,7 +62,7 @@ const store = new Vuex.Store({
         const { data } = await api.addEvent(newEvent)
         return data
       } catch (e) {
-        return (e.response && e.response.data) || e
+        return createError(e)
       }
     },
     async updateEventById ({ commit }, event) {
@@ -66,7 +71,7 @@ const store = new Vuex.Store({
         const { data } = await api.updateEventById(id, event)
         return data
       } catch (e) {
-        return (e.response && e.response.data) || e
+        return createError(e)
       }
     },
     async updateEventDateById ({ commit }, { id, start, end }) {
@@ -74,7 +79,7 @@ const store = new Vuex.Store({
         const { data } = await api.updateEventDateById(id, { start, end })
         return data
       } catch (e) {
-        return (e.response && e.response.data) || e
+        return createError(e)
       }
     },
     async deleteEventById ({ commit }, id) {
@@ -82,7 +87,7 @@ const store = new Vuex.Store({
         const { data } = await api.deleteEventById(id)
         return data
       } catch (e) {
-        return (e.response && e.response.data) || e
+        return createError(e)
       }
     }
   }
